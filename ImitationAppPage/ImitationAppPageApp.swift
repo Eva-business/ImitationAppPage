d5caae64c3f94c35285ec38cdebeba5037757b7e
GaryHu <garyhu17@gmail.com>
2025-09-30 16:22:04 +0800

    Initial Commit

---
+++ ImitationAppPage/ImitationAppPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImitationAppPageApp.swift
+//  ImitationAppPage
+//
+//  Created by user13 on 2025/9/30.
+//
+
+import SwiftUI
+
+@main
+struct ImitationAppPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
